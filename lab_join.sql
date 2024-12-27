68f361ea3b82c3a682909265e06a130a94279e7b
TheLazyCactus <perraud.valentin@yahoo.fr>
2024-12-27 11:51:07 +0100

    Create lab_join.sql

---
+++ lab_join.sql
@@ -0,0 +1,83 @@
+Use sakila;
+SELECT 
+    c.name AS cat_name, COUNT(fc.film_id) AS film_per_category
+FROM
+    category c
+        JOIN
+    film_category fc ON c.category_id = fc.category_id
+GROUP BY c.name;
+
+SELECT 
+    s.store_id, c.city, co.country
+FROM
+    store s
+        JOIN
+    address a ON s.address_id = a.address_id
+        JOIN
+    city c ON a.city_id = c.city_id
+        JOIN
+    country co ON c.country_id = co.country_id;
+
+SELECT 
+s.store_id,
+SUM(p.amount) AS total_revenue
+FROM store s
+JOIN
+	staff st ON s.store_id = st.store_id
+JOIN 
+	payment p ON st.staff_id = p.staff_id
+GROUP BY store_id;
+
+SELECT
+c.name AS category,
+ROUND(AVG(f.length),0) AS avg_running_time
+FROM
+	category c
+JOIN
+	film_category fc ON c.category_id = fc.category_ID
+JOIN
+	film f ON fc.film_id = f.film_id
+GROUP BY c.name;
+
+SELECT
+c.name AS category,
+ROUND(AVG(f.length),0) AS avg_running_time
+FROM
+	category c
+JOIN
+	film_category fc ON c.category_id = fc.category_ID
+JOIN
+	film f ON fc.film_id = f.film_id
+GROUP BY c.name
+ORDER BY avg_running_time DESC;
+
+SELECT 
+	f.title,
+    COUNT(rental_id) AS number_rent
+FROM film f
+JOIN
+	inventory i ON f.film_id = i.film_id
+JOIN
+	rental r ON i.inventory_id = r.inventory_id
+ GROUP BY f.title   
+ORDER BY number_rent DESC
+LIMIT 10;
+
+SELECT
+f.title,
+s.store_id
+FROM film f
+JOIN inventory i ON f.film_id = i.film_id
+JOIN store s ON i.store_id = i.store_id
+WHERE f.title ='Academy Dinosaur'
+GROUP BY store_id;
+
+SELECT
+DISTINCT(f.title) AS film_title,
+CASE
+	WHEN IFNULL(COUNT(i.inventory_id),0) > 0 THEN 'Available'
+    ELSE 'NOT Available'
+END AS availability
+FROM film f
+LEFT JOIN inventory i ON f.film_id=i.film_id
+GROUP BY f.title    
